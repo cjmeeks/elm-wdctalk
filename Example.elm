da6064e01cb46e924fc14a2bad6f3ae1bfcdedf6
cj meeks <cj meeks>
2016-09-26 18:36:03 -0500

    Added slides

--- Example.elm
+++ Example.elm
@@ -12,38 +12,28 @@ main =
     }
 
 -- MODEL
-
-
 type alias Model = Int
-
 
 model : Model
 model =
   0
-
-
-
 -- UPDATE
-
-
 type Msg
   = Increment
   | Decrement
-
+  | Reset
 
 update : Msg -> Model -> Model
 update msg model =
   case msg of
     Increment ->
       model + 1
-
     Decrement ->
       model - 1
-
-
+    Reset ->
+      0
 
 -- VIEW
-
 
 view : Model -> Html Msg
 view model =
@@ -51,8 +41,8 @@ view model =
     [ button [ onClick Decrement ] [ text "-" ]
     , div [ countStyle ] [ text (toString model) ]
     , button [ onClick Increment ] [ text "+" ]
+    , button [ onClick Reset ] [ text "Reset" ]
     ]
-
 
 countStyle : Attribute msg
 countStyle =
